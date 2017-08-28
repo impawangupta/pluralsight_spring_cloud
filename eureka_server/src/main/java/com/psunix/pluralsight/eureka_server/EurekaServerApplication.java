f328da499aaedd7994b159aae504739c1d1b0d3d
Pawan Gupta <em.pawangupta@gmail.com>
2017-08-28 06:58:45 -0400

    Adds spring cloud Config server

--- eureka_server/src/main/java/com/psunix/pluralsight/eureka_server/EurekaServerApplication.java
+++ eureka_server/src/main/java/com/psunix/pluralsight/eureka_server/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.psunix.pluralsight.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaClient
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
